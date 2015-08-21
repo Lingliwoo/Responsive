0b614a7c729265e9642b27c9e1f1f68b777a22f5
Steven Liang <rockboyfor@gmail.com>
2015-08-21 11:38:09 +0800

    do some clean

--- script/demo.js
+++
@@ -1,7 +0,0 @@
-				$(function() {
-                    $("#menuIcon,nav ul li").click(function() {
-                        if ($("#menuIcon").is(":visible")) { //防止宽屏上点击
-                            $("nav ul").toggle(300);
-                        };
-                    });
-                })
